6415bd3778d4445358c7adf1e14f8790636c83ea
Ed McClanahan <ed@swiftstack.com>
2019-01-17 10:28:46 -0800

    Flushed out remainder of FUSE upcalls
    Also merged mount.go contents into fuse.go so all FUSE stuff goes there now.

--- pfsagentd/fuse.go
+++ pfsagentd/fuse.go
@@ -3,13 +3,128 @@ package main
 import (
 	"io"
 	"os"
+	"os/exec"
+	"path"
 	"reflect"
 	"syscall"
 	"time"
 
 	"bazil.org/fuse"
+
 	"github.com/swiftstack/ProxyFS/utils"
 )
+
+const (
+	maxUnmountRetryCount uint32 = 100
+	unmountRetryGap             = 100 * time.Millisecond
+)
+
+func performMount() {
+	var (
+		curRetryCount                 uint32
+		err                           error
+		lazyUnmountCmd                *exec.Cmd
+		mountPointContainingDirDevice int64
+		mountPointDevice              int64
+	)
+
+	err = fuse.Unmount(globals.config.FUSEMountPointPath)
+	if nil != err {
+		logTracef("pre-fuse.Unmount() in performMount() returned: %v", err)
+	}
+
+	mountPointContainingDirDevice = fetchInodeDevice("path.Dir([Agent]FUSEMountPointPath", path.Dir(globals.config.FUSEMountPointPath))
+	mountPointDevice = fetchInodeDevice("[Agent]FUSEMountPointPath", globals.config.FUSEMountPointPath)
+
+	if mountPointDevice != mountPointContainingDirDevice {
+		// Presumably, the mount point is (still) currently mounted, so attempt to unmount it first
+
+		lazyUnmountCmd = exec.Command("fusermount", "-uz", globals.config.FUSEMountPointPath)
+		err = lazyUnmountCmd.Run()
+		if nil != err {
+			logFatal(err)
+		}
+
+		curRetryCount = 0
+
+		for mountPointDevice != mountPointContainingDirDevice {
+			time.Sleep(unmountRetryGap) // Try again in a bit
+			curRetryCount++
+			if curRetryCount >= maxUnmountRetryCount {
+				logFatalf("mountPointDevice != mountPointContainingDirDevice MaxRetryCount exceeded")
+			}
+			mountPointDevice = fetchInodeDevice("[Agent]FUSEMountPointPath", globals.config.FUSEMountPointPath)
+		}
+	}
+
+	globals.fuseConn, err = fuse.Mount(
+		globals.config.FUSEMountPointPath,
+		fuse.AllowOther(),
+		fuse.AsyncRead(),
+		fuse.DefaultPermissions(), // so handleAccessRequest() should not be called
+		fuse.ExclCreate(),
+		fuse.FSName(globals.config.FUSEVolumeName),
+		fuse.NoAppleDouble(),
+		fuse.NoAppleXattr(),
+		fuse.ReadOnly(),
+		fuse.Subtype("ProxyFS"),
+		fuse.VolumeName(globals.config.FUSEVolumeName),
+	)
+	if nil != err {
+		logFatal(err)
+	}
+
+	<-globals.fuseConn.Ready
+	if nil != globals.fuseConn.MountError {
+		logFatal(globals.fuseConn.MountError)
+	}
+
+	logInfof("%s mounted", globals.config.FUSEMountPointPath)
+
+	go serveFuse()
+}
+
+func fetchInodeDevice(pathTitle string, path string) (inodeDevice int64) {
+	var (
+		err  error
+		fi   os.FileInfo
+		ok   bool
+		stat *syscall.Stat_t
+	)
+
+	fi, err = os.Stat(path)
+	if nil != err {
+		if os.IsNotExist(err) {
+			logFatalf("%s path (%s) not found", pathTitle, path)
+		} else {
+			logFatalf("%s path (%s) os.Stat() failed: %v", pathTitle, path, err)
+		}
+	}
+	if nil == fi.Sys() {
+		logFatalf("%s path (%s) had empty os.Stat()", pathTitle, path)
+	}
+	stat, ok = fi.Sys().(*syscall.Stat_t)
+	if !ok {
+		logFatalf("%s path (%s) fi.Sys().(*syscall.Stat_t) returned !ok", pathTitle, path)
+	}
+
+	inodeDevice = int64(stat.Dev)
+
+	return
+}
+
+func performUnmount() {
+	var (
+		err error
+	)
+
+	err = fuse.Unmount(globals.config.FUSEMountPointPath)
+	if nil != err {
+		logFatal(err)
+	}
+
+	logInfof("%s unmounted", globals.config.FUSEMountPointPath)
+}
 
 func serveFuse() {
 	var (
@@ -31,20 +146,62 @@ func serveFuse() {
 		}
 		logTracef("serveFuse() got %v", reflect.ValueOf(request).Type())
 		switch request.(type) {
+		case *fuse.AccessRequest:
+			handleAccessRequest(request.(*fuse.AccessRequest))
+		case *fuse.CreateRequest:
+			handleCreateRequest(request.(*fuse.CreateRequest))
 		case *fuse.DestroyRequest:
 			handleDestroyRequest(request.(*fuse.DestroyRequest))
+		case *fuse.ExchangeDataRequest:
+			handleExchangeDataRequest(request.(*fuse.ExchangeDataRequest))
+		case *fuse.FlushRequest:
+			handleFlushRequest(request.(*fuse.FlushRequest))
+		case *fuse.ForgetRequest:
+			handleForgetRequest(request.(*fuse.ForgetRequest))
+		case *fuse.FsyncRequest:
+			handleFsyncRequest(request.(*fuse.FsyncRequest))
 		case *fuse.GetattrRequest:
 			handleGetattrRequest(request.(*fuse.GetattrRequest))
+		case *fuse.GetxattrRequest:
+			handleGetxattrRequest(request.(*fuse.GetxattrRequest))
 		case *fuse.InitRequest:
 			handleInitRequest(request.(*fuse.InitRequest))
 		case *fuse.InterruptRequest:
 			handleInterruptRequest(request.(*fuse.InterruptRequest))
+		case *fuse.LinkRequest:
+			handleLinkRequest(request.(*fuse.LinkRequest))
+		case *fuse.ListxattrRequest:
+			handleListxattrRequest(request.(*fuse.ListxattrRequest))
 		case *fuse.LookupRequest:
 			handleLookupRequest(request.(*fuse.LookupRequest))
+		case *fuse.MkdirRequest:
+			handleMkdirRequest(request.(*fuse.MkdirRequest))
+		case *fuse.MknodRequest:
+			handleMknodRequest(request.(*fuse.MknodRequest))
 		case *fuse.OpenRequest:
 			handleOpenRequest(request.(*fuse.OpenRequest))
+		case *fuse.ReadRequest:
+			handleReadRequest(request.(*fuse.ReadRequest))
+		case *fuse.ReadlinkRequest:
+			handleReadlinkRequest(request.(*fuse.ReadlinkRequest))
+		case *fuse.ReleaseRequest:
+			handleReleaseRequest(request.(*fuse.ReleaseRequest))
+		case *fuse.RemoveRequest:
+			handleRemoveRequest(request.(*fuse.RemoveRequest))
+		case *fuse.RemovexattrRequest:
+			handleRemovexattrRequest(request.(*fuse.RemovexattrRequest))
+		case *fuse.RenameRequest:
+			handleRenameRequest(request.(*fuse.RenameRequest))
+		case *fuse.SetattrRequest:
+			handleSetattrRequest(request.(*fuse.SetattrRequest))
+		case *fuse.SetxattrRequest:
+			handleSetxattrRequest(request.(*fuse.SetxattrRequest))
 		case *fuse.StatfsRequest:
 			handleStatfsRequest(request.(*fuse.StatfsRequest))
+		case *fuse.SymlinkRequest:
+			handleSymlinkRequest(request.(*fuse.SymlinkRequest))
+		case *fuse.WriteRequest:
+			handleWriteRequest(request.(*fuse.WriteRequest))
 		default:
 			logWarnf("recieved unserviced %v", reflect.ValueOf(request).Type())
 			request.RespondError(fuse.ENOTSUP)
@@ -52,9 +209,53 @@ func serveFuse() {
 	}
 }
 
+func handleAccessRequest(request *fuse.AccessRequest) {
+	logFatalf("handleAccessRequest() should not have been called due to DefaultPermissions() passed to fuse.Mount()")
+}
+
+func handleCreateRequest(request *fuse.CreateRequest) {
+	logInfof("TODO: handleCreateRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
 func handleDestroyRequest(request *fuse.DestroyRequest) {
 	logInfof("TODO: handleDestroyRequest()")
 	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleExchangeDataRequest(request *fuse.ExchangeDataRequest) {
+	logInfof("TODO: handleExchangeDataRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleFlushRequest(request *fuse.FlushRequest) {
+	logInfof("TODO: handleFlushRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleForgetRequest(request *fuse.ForgetRequest) {
+	logInfof("TODO: handleForgetRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleFsyncRequest(request *fuse.FsyncRequest) {
+	logInfof("TODO: handleFsyncRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
 	logInfof("Responding with fuse.ENOTSUP")
 	request.RespondError(fuse.ENOTSUP)
 }
@@ -95,28 +296,36 @@ func handleGetattrRequest(request *fuse.GetattrRequest) {
 	}
 }
 
-func handleInitRequest(request *fuse.InitRequest) {
-	var (
-		response *fuse.InitResponse
-	)
-
-	logWarnf("handleInitRequest() should not have been called... fuse.Mount() supposedly took care of it")
-
-	logInfof("TODO: handleInitRequest()")
+func handleGetxattrRequest(request *fuse.GetxattrRequest) {
+	logInfof("TODO: handleGetxattrRequest()")
 	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
 	logInfof("Payload\n%s", utils.JSONify(request, true))
-	response = &fuse.InitResponse{
-		Library:      request.Kernel,
-		MaxReadahead: request.MaxReadahead,
-		Flags:        request.Flags,
-		MaxWrite:     uint32(1024 * 1024 * 1024),
-	}
-	logInfof("resonding with:\n%s", utils.JSONify(response, true))
-	request.Respond(response)
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleInitRequest(request *fuse.InitRequest) {
+	logFatalf("handleInitRequest() should not have been called... fuse.Mount() supposedly took care of it")
 }
 
 func handleInterruptRequest(request *fuse.InterruptRequest) {
 	logInfof("TODO: handleInterruptRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleLinkRequest(request *fuse.LinkRequest) {
+	logInfof("TODO: handleLinkRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleListxattrRequest(request *fuse.ListxattrRequest) {
+	logInfof("TODO: handleListxattrRequest()")
 	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
 	logInfof("Payload\n%s", utils.JSONify(request, true))
 	logInfof("Responding with fuse.ENOTSUP")
@@ -131,8 +340,88 @@ func handleLookupRequest(request *fuse.LookupRequest) {
 	request.RespondError(fuse.ENOTSUP)
 }
 
+func handleMkdirRequest(request *fuse.MkdirRequest) {
+	logInfof("TODO: handleMkdirRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleMknodRequest(request *fuse.MknodRequest) {
+	logInfof("TODO: handleMknodRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
 func handleOpenRequest(request *fuse.OpenRequest) {
 	logInfof("TODO: handleOpenRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleReadRequest(request *fuse.ReadRequest) {
+	logInfof("TODO: handleReadRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleReadlinkRequest(request *fuse.ReadlinkRequest) {
+	logInfof("TODO: handleReadlinkRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleReleaseRequest(request *fuse.ReleaseRequest) {
+	logInfof("TODO: handleReleaseRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleRemoveRequest(request *fuse.RemoveRequest) {
+	logInfof("TODO: handleRemoveRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleRemovexattrRequest(request *fuse.RemovexattrRequest) {
+	logInfof("TODO: handleRemovexattrRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleRenameRequest(request *fuse.RenameRequest) {
+	logInfof("TODO: handleRenameRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleSetattrRequest(request *fuse.SetattrRequest) {
+	logInfof("TODO: handleSetattrRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleSetxattrRequest(request *fuse.SetxattrRequest) {
+	logInfof("TODO: handleSetxattrRequest()")
 	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
 	logInfof("Payload\n%s", utils.JSONify(request, true))
 	logInfof("Responding with fuse.ENOTSUP")
@@ -158,4 +447,20 @@ func handleStatfsRequest(request *fuse.StatfsRequest) {
 	}
 	logInfof("resonding with:\n%s", utils.JSONify(response, true))
 	request.Respond(response)
+}
+
+func handleSymlinkRequest(request *fuse.SymlinkRequest) {
+	logInfof("TODO: handleSymlinkRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
+}
+
+func handleWriteRequest(request *fuse.WriteRequest) {
+	logInfof("TODO: handleWriteRequest()")
+	logInfof("Header:\n%s", utils.JSONify(request.Header, true))
+	logInfof("Payload\n%s", utils.JSONify(request, true))
+	logInfof("Responding with fuse.ENOTSUP")
+	request.RespondError(fuse.ENOTSUP)
 }
